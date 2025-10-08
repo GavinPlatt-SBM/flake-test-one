ccfe6576f737669fadffe45a905d18069d5e0623
GavinPlatt-SBM <gavin.p@smithbrothersmedia.com.au>
2025-10-08 12:15:25 +1100

    fix flake.nix

--- flake.nix
+++ flake.nix
@@ -11,26 +11,27 @@
       pkgs = nixpkgs.legacyPackages.${system};
 
       nextjsApp = pkgs.stdenv.mkDerivation {
-        name = "nextjs-app";
+        pname = "nextjs-app";
+        version = "0.1.0";
         src = ./.;
 
         nativeBuildInputs = [ pkgs.nodejs pkgs.yarn ];
 
-        installPhase = ''
-          mkdir -p $out
-          cp -r . $out
-          cd $out
+        buildPhase = ''
+          export HOME=$(mktemp -d)
+          yarn config set strict-ssl false
           yarn install
+          yarn build
         '';
 
-        buildPhase = ''
-          yarn build
+        installPhase = ''
+          mkdir -p $out
+          cp -r . $out/
         '';
       };
     in {
-      packages.${system} = {
-        nextjsApp = nextjsApp;
-        default = nextjsApp;
-      };
+      packages.nextjsApp = nextjsApp;
+      packages.default = nextjsApp;
     });
 }
+
